08ef924a36e1686488d5d49050e97e22f8a0d7e5
nahomey <nahiob2@gmail.com>
2021-03-12 20:46:24 -0600

    assignment

---
+++ W3D5/alarm.js
@@ -0,0 +1,12 @@
+function alarm() {
+    let colorChange = document.getElementById("highlighted");
+    let min = parseInt(document.getElementById("first").value);
+    let sec = parseInt(document.getElementById("second").value);
+    let result = (min * 60) + (sec)
+    let final = result * 1000;
+    colorChange.style.backgroundColor = "red"
+    let a = setInterval(() => {
+        colorChange.style.backgroundColor = "white"
+    }, final);
+
+}
