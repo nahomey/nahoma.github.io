1ff068e8372d17af2661a3f0a78c499735b05510
nahomey <nahiob2@gmail.com>
2021-08-31 14:18:35 -0500

    testing

--- W3D5/calculator.js
+++
@@ -1,6 +0,0 @@
-function calculator() {
-    let c = parseInt(document.getElementById("first").value);
-    let d = parseInt(document.getElementById("second").value);
-    let result = c * d / 100;
-    document.getElementById('result').value = result
-}
