4bba050c0cbbd695030447065ec84077152c7d01
nahomey <nahiob2@gmail.com>
2021-09-02 14:52:27 -0500

    CS305 labs

---
+++ w1d4arrays/multidimension.js
@@ -0,0 +1,18 @@
+"use strict ";
+
+let matrix = [
+    [1, 2],
+    [4, 5]
+];
+
+function sumMatrix(matrix) {
+
+    let sum = 0;
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix.length; j++) {
+            console.log(sumMatrix[i][j]);
+            sum += arr[i][j];
+        }
+    }
+    return sum;
+}
