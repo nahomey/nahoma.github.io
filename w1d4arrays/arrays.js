9e89f9f8561af84364ec37d463d3dd64e17a8aa4
nahomey <nahiob2@gmail.com>
2021-09-02 21:28:55 -0500

    ArrayAssignment

---
+++ w1d4arrays/arrays.js
@@ -0,0 +1,98 @@
+"use strict";
+
+/* You need the module.exports when testing in node.  Comment it out when you send your file to the browser
+ */
+module.exports = { maxOfThree, sum, multiply, findLongestWord, reverseArray, reverseArrayInPlace }; //add all of your function names here that you need for the node mocha tests
+
+/**
+ * 
+ * @param {number} aA1 is a number
+ * @param {number} bB1 is a number 
+ * @param {number} cC1 is a number
+ * @returns {number} largest of a, b, c
+ */
+function maxOfThree(aA1, bB1, cC1) {
+    if (aA1 >= bB1 && aA1 >= cC1) {
+        return aA1;
+    } else if (bB1 >= aA1 && bB1 >= cC1) {
+        return bB1;
+    } else
+        return cC1;
+}
+
+/**
+ * 
+ * @param {Array} arr of numbers
+ * @returns {number} sum of arr numbers
+ */
+function sum(arr) {
+    let tot = 0;
+    for (const number of arr) {
+        tot += number;
+    }
+    return tot;
+}
+
+
+/**
+ * 
+ * @param {Array} arr of numbers
+ * @returns {number} sum of arr numbers
+ */
+function multiply(arr) {
+    let tot = 1;
+    for (const number of arr) {
+        tot *= number;
+    }
+
+    return tot;
+
+}
+/**
+ * 
+ */
+
+
+function findLongestWord(arr) {
+    let longest = arr[0].length;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].length > longest) {
+            longest = arr[i].length;
+        }
+    }
+    return longest;
+}
+console.log(findLongestWord(["abel", "nahom", " azamit"]));
+/**
+ * 
+ * @param {arr} array to be reversed 
+ * @returns (arr) reversed array
+ */
+
+function reverseArray(arr) {
+    let reversed = [];
+    for (let i = arr.length - 1; i >= 0; i--) {
+        reversed.push(arr[i]);
+    }
+    return reversed;
+}
+console.log(reverseArray(["A", "B", "C"]));
+
+/**
+ * 
+ * @param {arr} array to be reversed in same array
+ * @returns {arr} reversed array in same place
+ */
+function reverseArrayInPlace(arr) {
+    let temp = [];
+    let len = arr.length;
+    for (let i = 0; i < len; i++) {
+
+        temp.push(arr.pop());
+    }
+    for (let i = 0; i < len; i++) {
+        arr.unshift(temp.pop());
+    }
+    return arr;
+}
+//console.log(reverseArrayInSamePlace(["A", "B", "C"]));
--- w1d4arrays/multidimension.js
+++ w1d4arrays/multidimension.js
@@ -1,18 +1,32 @@
 "use strict ";
 
-let matrix = [
-    [1, 2],
-    [4, 5]
-];
+// let matrix = [
+//     [1, 2],
+//     [4, 5]
+// ];
 
-function sumMatrix(matrix) {
+// function sumMatrix(matrix) {
 
-    let sum = 0;
-    for (let i = 0; i < matrix.length; i++) {
-        for (let j = 0; j < matrix.length; j++) {
-            console.log(sumMatrix[i][j]);
-            sum += arr[i][j];
+//     let sum = 0;
+//     for (let i = 0; i < matrix.length; i++) {
+//         for (let j = 0; j < matrix.length; j++) {
+//             console.log(sumMatrix[i][j]);
+//             sum += arr[i][j];
+//         }
+//     }
+//     return sum;
+// }
+
+
+
+function findLongestWord(arr) {
+    let words = "";
+    let longest = arr[i].length;
+    for (let i = 0; i < words.length; i++) {
+        if (words[i].length > longest) {
+            arr[i] = longest;
         }
     }
-    return sum;
+    return longest;
 }
+console.log(findLongestWord(["abel", "nahom", "azamit"]));
