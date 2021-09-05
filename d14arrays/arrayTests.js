16257ff1f218407a271ff3cfefd3e442fc420e8e
nahomey <nahiob2@gmail.com>
2021-09-05 13:28:35 -0500

    Array

--- d14arrays/arrayTests.js
+++ d14arrays/arrayTests.js
@@ -3,7 +3,7 @@
 /* You need the assert and function name declarations to test in node.  
 Comment these out when you send it to the browser with the index.html mocha setup page.
 */
-// //const assert = require("assert");
+const assert = require("assert");
 //always need this with node
 // const myExports = require("./arrays");
 //with node need the name of your file with your functions here
