35caa909ed61f62878bc50c37600baac8ba56ced
nahomey <nahiob2@gmail.com>
2021-09-05 12:41:11 -0500

    Array

--- w1d1/Assignment/w1d2Q6.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-function calcDistance(x1, y1, x2, y2) {
-
-    let d = Math.sqrt(((x2 - x1) ** 2) + ((y2 - y1) ** 2));
-    return d;
-}
-console.log("expect 5: ", calcDistance(0, 0, 5, 5));
--- w1d2/assignment2/Q6.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-function calcDistance(x1, y1, x2, y2) {
-
-    let d = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
-    return d;
-}
-console.log("expect 5: ", calcDistance(0, 0, 5, 5));
