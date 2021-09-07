77035463f61d05df882bf078aa98502f8a92af99
nahomey <nahiob2@gmail.com>
2021-09-07 00:10:52 -0500

    objects

--- d21objects/libraryBooksTes.js
+++ d21objects/libraryBooksTes.js
@@ -1,7 +1,7 @@
 "use strict";
 
 /*  comment out the exports when running in the browser */
-const assert = require("assert"); //always need this with node
+//const assert = require("assert"); //always need this with node
 // const myExports = require("./app.js"); //with node need the name of your file with your functions here
 // const findTitles = myExports.findTitles; //do this for all of the functions used in the Mocha tests
 // const findAuthors = myExports.findAuthors;
