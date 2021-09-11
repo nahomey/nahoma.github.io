59667f94c9d51b4a65151650b0e1d11306140a0f
nahomey <nahiob2@gmail.com>
2021-09-10 21:28:26 -0500

    array assignement

---
+++ d25destructure/arrayPractice.js
@@ -0,0 +1,49 @@
+"use strict"
+/* eslint-disable */
+/* You need the module.exports when testing in node.  Comment it out when you send your file to the browser */
+module.exports = { doubleNums, doubleAges, filterEven, filterOver10, findEvenNum, findEvenAge, includesEvenNum, includesEvenAge }; //add all of your function names here that you need for the node mocha tests
+
+
+function doubleNums(arr) {
+    return arr.map(num => num * 2);
+}
+
+function doubleAges(arr) {
+    return arr.map(item => ({ name: item.name, age: item.age * 2 }));
+
+};
+
+
+
+function filterEven(arr) {
+    return arr.filter(item => item % 2 === 0);
+
+}
+
+function filterOver10(arr) {
+    return arr.filter(item => item.age > 10);
+
+}
+
+function findEvenNum(arr) {
+    return arr.find(item => item % 2 == 0);
+
+}
+
+function findEvenAge(arr) {
+    return arr.find(item => item.age % 2 == 0);
+
+}
+
+function includesEvenNum(arr) {
+    return arr.includes(item => item % 2 == 0);
+
+}
+
+function includesEvenAge(arr) {
+    let result = arr.find(item => item % 2 == 0);
+    if (result == undefned) return false;
+    return true;
+
+
+}
