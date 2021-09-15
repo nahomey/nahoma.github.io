57872bfddc87cbbcc8c03a08673d358bc32ad586
nahomey <nahiob2@gmail.com>
2021-09-14 21:08:39 -0500

    recursion Assignment

--- d31recursion/recursionTests.js
+++ d31recursion/recursionTests.js
@@ -4,11 +4,11 @@
 /* You need the assert and function name declarations to test in node.  
 Comment these out when you send it to the browser with the index.html mocha setup page.
 */
-const assert = require("assert"); //always need this with node
-const myExports = require("./recursion.js"); //with node need the name of your file with your functions here
-const sumTo = myExports.sumTo; //do this for all of the functions used in the Mocha tests
-const factorial = myExports.factorial;
-const fibonacci = myExports.fibonacci;
+// const assert = require("assert"); //always need this with node
+// const myExports = require("./recursion.js"); //with node need the name of your file with your functions here
+// const sumTo = myExports.sumTo; //do this for all of the functions used in the Mocha tests
+// const factorial = myExports.factorial;
+// const fibonacci = myExports.fibonacci;
 
 /* the following are for Thursday */
 const outputList = myExports.outputList;
