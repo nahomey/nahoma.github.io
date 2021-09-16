b131cec380a55d0d0490c6ce668db7e494dea4a3
nahomey <nahiob2@gmail.com>
2021-09-15 21:20:11 -0500

    spread Assignement

--- d31recursion/recursion.js
+++ d31recursion/recursion.js
@@ -2,7 +2,7 @@
 
 /* You need the module.exports when testing in node.  Comment it out when you send your file to the browser 
  */
-// module.exports = { sumTo, factorial, fibonacci }; //add all of your function names here that you need for the node mocha tests
+module.exports = { sumTo, factorial, fibonacci, outputList, outputListLoop, reverseList, reverseListLoop }; //add all of your function names here that you need for the node mocha tests
 
 /**
  * 
@@ -32,4 +32,61 @@ function fibonacci(num) {
     if (num < 2) return num;
     return fibonacci(num - 1) + fibonacci(num - 2);
 
+}
+
+/**
+ * 
+ * @param {object} input is array of objects
+ * @returns {string} returns string;
+ */
+function outputList(input) {
+    if (input.next === null) {
+        return input.value + " printed to console";
+    } else {
+        return input.value + " " + outputList(input.next);
+    }
+}
+/**
+ * 
+ * @param {object} input to printed out
+ * @returns {string} returns string
+ */
+function outputListLoop(input) {
+    let str = "";
+    while (input.next !== null) {
+
+        str += input.value + " ";
+        input = input.next;
+    }
+
+    return str + input.value + " printed to console";
+}
+
+/**
+ * 
+ * @param {object} list  to printed out reversedly
+ * @returns {string} returns string
+ */
+function reverseList(list) {
+    if (list.next == null)
+        return list.value;
+    else {
+        return reverseList(list.next.value);
+
+    }
+}
+/**
+ * 
+ * @param {object} list to be reversed
+ * @returns{string} returns string
+ */
+function reverseListLoop(list) {
+    let arr = []
+    while (list) {
+        arr.push(list.value);
+        list = list.next;
+    }
+    for (let value of arr.reverse()) {
+        console.log(value);
+    }
 }
--- d31recursion/recursionTests.js
+++ d31recursion/recursionTests.js
@@ -4,32 +4,32 @@
 /* You need the assert and function name declarations to test in node.  
 Comment these out when you send it to the browser with the index.html mocha setup page.
 */
-// const assert = require("assert"); //always need this with node
-// const myExports = require("./recursion.js"); //with node need the name of your file with your functions here
-// const sumTo = myExports.sumTo; //do this for all of the functions used in the Mocha tests
-// const factorial = myExports.factorial;
-// const fibonacci = myExports.fibonacci;
+const assert = require("assert"); //always need this with node
+const myExports = require("./recursion.js"); //with node need the name of your file with your functions here
+const sumTo = myExports.sumTo; //do this for all of the functions used in the Mocha tests
+const factorial = myExports.factorial;
+const fibonacci = myExports.fibonacci;
 
 /* the following are for Thursday */
-// const outputList = myExports.outputList;
-// const outputListLoop = myExports.outputListLoop;
-// const reverseList = myExports.reverseList;
-// const reverseListLoop = myExports.reverseListLoop;
+const outputList = myExports.outputList;
+const outputListLoop = myExports.outputListLoop;
+const reverseList = myExports.reverseList;
+const reverseListLoop = myExports.reverseListLoop;
 
 
-// let list = {
-//     value: 1,
-//     next: {
-//         value: 2,
-//         next: {
-//             value: 3,
-//             next: {
-//                 value: 4,
-//                 next: null
-//             }
-//         }
-//     }
-// };
+let list = {
+    value: 1,
+    next: {
+        value: 2,
+        next: {
+            value: 3,
+            next: {
+                value: 4,
+                next: null
+            }
+        }
+    }
+};
 
 describe("recursion tests", function() {
     it("tests summing by recursion", function() {
@@ -47,18 +47,18 @@ describe("recursion tests", function() {
     it("tests fibonacci 30", function() {
         assert.strictEqual(fibonacci(30), 832040);
     });
-    // it("recursive version prints 1 2 3 4 to console", function() {
-    //     assert.strictEqual(outputList(list), "1 2 3 4 printed to console");
-    // });
-    // it("iterative version prints 1 2 3 4 to console", function() {
-    //     assert.strictEqual(outputListLoop(list), "1 2 3 4 printed to console");
-    // });
+    it("recursive version prints 1 2 3 4 to console", function() {
+        assert.strictEqual(outputList(list), "1 2 3 4 printed to console");
+    });
+    it("iterative version prints 1 2 3 4 to console", function() {
+        assert.strictEqual(outputListLoop(list), "1 2 3 4 printed to console");
+    });
 
-    // it("recursive version prints 4 3 2 1 to console", function() {
-    //     assert.strictEqual(reverseList(list), "4 3 2 1 printed to console");
-    // });
-    // it("loop version prints 4 3 2 1 to console", function() {
-    //     assert.strictEqual(reverseListLoop(list), "4 3 2 1 printed to console");
-    // });
+    it("recursive version prints 4 3 2 1 to console", function() {
+        assert.strictEqual(reverseList(list), "4 3 2 1 printed to console");
+    });
+    it("loop version prints 4 3 2 1 to console", function() {
+        assert.strictEqual(reverseListLoop(list), "4 3 2 1 printed to console");
+    });
 
 });
