6ec479e0ae730713af724bcdd0bef6006db6e9f9
nahomey <nahiob2@gmail.com>
2021-09-17 17:47:19 -0500

    closure assignment

--- d33closures/closures.js
+++ d33closures/closures.js
@@ -42,21 +42,30 @@ function inBetween(low, high) {
      */
     function byField(fieldName) {
         return function(a, b) {
-            return a[fieldName] > b[fieldName] ? 1 : -1;
-        }
-
+            if (a[fieldName] > b[fieldName]) {
+                return 1;
+            } else if (a[fieldName] < b[fieldName]) {
+                return -1;
+            }
+        };
     }
-
 
 
     /**
      * @returns {Function} closure that returns it's number
      */
     function makeArmy() {
-
+        let arr = [];
+        for (let i = 0; i < 10; i++) {
+            let show = function() {
+                return i;
+            };
+            arr.push(show);
+        }
+        return arr;
     }
 
-    let army = makeArmy();
+
 
     //army[0](); // the shooter number 0 shows 10
     //army[5](); // and number 5 also outputs 10...
